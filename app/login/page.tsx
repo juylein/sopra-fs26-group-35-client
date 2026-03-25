1917c061e059e53af716f6b08ed9f461c6f3022c
Julie <julie.dao@hotmail.com>
2026-03-25 15:10:15 +0100

    feat: edited the design of the landing and login page #1

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,44 +1,36 @@
 "use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
 
-import { useRouter } from "next/navigation"; // use NextJS router for navigation
+import { useRouter } from "next/navigation";
 import { useApi } from "@/hooks/useApi";
 import useLocalStorage from "@/hooks/useLocalStorage";
 import { User } from "@/types/user";
 import { Button, Form, Input } from "antd";
-// Optionally, you can import a CSS module or file for additional styling:
-// import styles from "@/styles/page.module.css";
 
-interface FormFieldProps {
-  label: string;
-  value: string;
+interface LoginFormValues {
+  username: string;
+  password: string;
 }
 
 const Login: React.FC = () => {
   const router = useRouter();
   const apiService = useApi();
   const [form] = Form.useForm();
-  // useLocalStorage hook example use
-  // The hook returns an object with the value and two functions
-  // Simply choose what you need from the hook:
   const {
-    // value: token, // is commented out because we do not need the token value
-    set: setToken, // we need this method to set the value of the token to the one we receive from the POST request to the backend server API
-    // clear: clearToken, // is commented out because we do not need to clear the token when logging in
-  } = useLocalStorage<string>("token", ""); // note that the key we are selecting is "token" and the default value we are setting is an empty string
-  // if you want to pick a different token, i.e "usertoken", the line above would look as follows: } = useLocalStorage<string>("usertoken", "");
+    set: setToken,
+  } = useLocalStorage<string>("token", "");
 
-  const handleLogin = async (values: FormFieldProps) => {
+  const { set: setUserId } = useLocalStorage<string>("id", "")
+
+  const handleLogin = async (values: LoginFormValues) => {
     try {
-      // Call the API service and let it handle JSON serialization and error handling
-      const response = await apiService.post<User>("/users", values);
+      const response = await apiService.post<User>("/users/login", values);
 
-      // Use the useLocalStorage hook that returned a setter function (setToken in line 41) to store the token if available
       if (response.token) {
         setToken(response.token);
       }
+      if (response.id) { setUserId(String(response.id))}
+      router.push(`/users/${response.id}`);
 
-      // Navigate to the user overview
-      router.push("/users");
     } catch (error) {
       if (error instanceof Error) {
         alert(`Something went wrong during the login:\n${error.message}`);
@@ -49,36 +41,39 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="login-container">
-      <Form
-        form={form}
-        name="login"
-        size="large"
-        variant="outlined"
-        onFinish={handleLogin}
-        layout="vertical"
-      >
-        <Form.Item
-          name="username"
-          label="Username"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Input placeholder="Enter username" />
-        </Form.Item>
-        <Form.Item
-          name="name"
-          label="Name"
-          rules={[{ required: true, message: "Please input your name!" }]}
-        >
-          <Input placeholder="Enter name" />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit" className="login-button">
-            Login
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+      <div className="login-page">
+        <div className="login-container">
+          <Form
+              form={form}
+              name="login"
+              size="large"
+              variant="outlined"
+              onFinish={handleLogin}
+              layout="vertical"
+          >
+            <Form.Item
+                name="username"
+                label="Username"
+                rules={[{ required: true, message: "Please input your username!" }]}
+            >
+              <Input placeholder="Enter username" />
+            </Form.Item>
+            <Form.Item
+                name="password"
+                label="Password"
+                rules={[{ required: true, message: "Please input your password!" }]}
+            >
+              <Input.Password placeholder="Enter password" />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="login-button">
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+        <a className="redirect-link" href="/register">New here? Register now</a>
+      </div>
   );
 };
 
