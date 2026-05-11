b4e5aa998f6a89b15f4a8e6f94545e158daa25ca
Le Minh Truong <truongleminh2001@gmail.com>
2026-05-11 10:52:35 +0200

    remove dupes

--- app/types/sessionParticipant 2.ts
+++
@@ -1,9 +0,0 @@
-export interface SessionParticipant {
-	id: string;
-	name: string;
-	initial: string;
-	color: string;
-	book: string;
-	page: number;
-	total: number;
-}
--- app/types/sessionParticipantGetDTO 2.ts
+++
@@ -1,9 +0,0 @@
-import Book from "@/books/page";
-import { User } from "./user";
-
-export interface SessionParticipantGetDTO {
-    shelfBookId: number;
-    pagesRead: number;
-    user: User;
-    book: Book;
-}
